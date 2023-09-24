3292ace182509e4ecad93f2b68d912aefd022d3d
Fex329 <118118769+Fex329@users.noreply.github.com>
2023-09-24 16:02:43 +0100

    Add files via upload

---
+++ BoardTicTacToe.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeExcercise
+{
+	internal class BoardTicTacToe
+	{
+		private char[,] _board = { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+
+		public BoardTicTacToe()
+		{
+			
+		}
+
+		public void Setfield()
+		{
+			Console.WriteLine("   |   |   ");
+			Console.WriteLine(" {0} | {1} | {2} ", _board[0, 0], _board[0, 1], _board[0, 2]);
+			Console.WriteLine("___|___|___");
+			Console.WriteLine("   |   |   ");
+			Console.WriteLine(" {0} | {1} | {2} ", _board[1, 0], _board[1, 1], _board[1, 2]);
+			Console.WriteLine("___|___|___");
+			Console.WriteLine("   |   |   ");
+			Console.WriteLine(" {0} | {1} | {2} ", _board[2, 0], _board[2, 1], _board[2, 2]);
+			Console.WriteLine("   |   |   ");
+
+
+		}
+
+			public void WriteSymbol(int selectedCell2, char playerSymbol)
+		{
+			switch (selectedCell2) 
+			{
+				case 1:
+					{ 
+						_board[0,0] = playerSymbol; 
+						break; 
+					}
+				case 2:
+					{
+						_board[0, 1] = playerSymbol;
+						break;
+					}
+				case 3:
+					{
+						_board[0, 2] = playerSymbol;
+						break;
+					}
+				case 4:
+					{
+						_board[1, 0] = playerSymbol;
+						break;
+					}
+				case 5:
+					{
+						_board[1, 1] = playerSymbol;
+						break;
+					}
+				case 6:
+					{
+						_board[1, 2] = playerSymbol;
+						break;
+					}
+				case 7:
+					{
+						_board[2, 0] = playerSymbol;
+						break;
+					}
+				case 8:
+					{
+						_board[2, 1] = playerSymbol;
+						break;
+					}
+				case 9:
+					{
+						_board[2, 2] = playerSymbol;
+						break;
+					}
+			}
+				Setfield();
+			}
+
+		public bool BoardChecker()
+		{
+			// here we perform horizontal and vertical checks
+			for (int i = 0; i < 3; i++)
+			{
+				if (_board[i, 0] == _board[i, 1] && _board[i, 1] == _board[i, 2])
+					return true;
+				if (_board[0, i] == _board[1, i] && _board[1, i] == _board[2, i])
+					return true;
+			}
+			// here diagonal checks 
+			if (_board[0, 0] == _board[1, 1] && _board[1, 1] == _board[2, 2])
+				return true;
+			if (_board[0, 2] == _board[1, 1] && _board[1, 1] == _board[2, 0])
+				return true;
+
+			return false;
+		}
+
+	}
+
+
+	
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System.Threading.Channels;
+
+namespace TicTacToeExcercise
+{
+	internal class Program
+	{
+
+		Console.WriteLine("Do you want to play TicTacToe?");
+
+
+		static BoardTicTacToe playBoard = new BoardTicTacToe();
+		static char player1Symbol = 'X';
+		static char player2Symbol = 'O';
+		static int turnsLeft = 9;
+		static bool hasWon = false;
+		static int[] numbersUsed = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		static void Main(string[] args)
+		{
+			playBoard.Setfield();
+			do
+			{
+				GetAndProcessInput("Player 1", player1Symbol);
+				if (playBoard.BoardChecker())
+				{
+					Console.WriteLine("Player 1 has won!");
+					Console.Read();
+					break;
+				}
+				turnsLeft--;
+				if (turnsLeft == 0)
+				{
+					Console.WriteLine("This looks like a draw!");
+					Console.Read();
+					break;
+				}
+
+				GetAndProcessInput("Player 2", player2Symbol);
+				if (playBoard.BoardChecker())
+				{
+					Console.WriteLine("Player 2 has won!");
+					Console.Read();
+					break;
+				}
+				turnsLeft--;
+
+
+
+			} while (turnsLeft > 0 && hasWon == false);
+
+
+
+		}
+
+		public static void GetAndProcessInput(string playerName, char playerSymbol)
+		{
+			//this method gets the input of a player and add the symbol on the corresponding cell
+
+			bool validEntry = false;
+			do
+			{
+				Console.WriteLine("Please {0}, select a free cell and enter the corresponding number", playerName);
+
+				string playerInput = Console.ReadLine();
+				int selectedCell;
+				
+
+				if (int.TryParse(playerInput, out selectedCell))
+				{
+					if (selectedCell > 0 && selectedCell < 10)
+					{
+						if (!numbersUsed.Contains(selectedCell))
+
+						{
+							Console.WriteLine("Cell already occupied");
+						}
+						else
+						{
+							playBoard.WriteSymbol(selectedCell, playerSymbol);
+							numbersUsed[selectedCell - 1] = 0;
+							validEntry = true;
+						}
+
+					}
+
+					else
+					{
+						Console.WriteLine("Number out of range, select a number between 1 and 9");
+					}
+				}
+				else if (playerInput == "")
+				{
+					Console.WriteLine("Empty input, try again!");
+				}
+
+				else
+				{
+					Console.WriteLine("Only numbers between 1 and 9 Please!");
+				}
+
+
+			} while (!validEntry);
+
+		}
+
+
+
+
+
+		/*public static bool BoardChecker(string[,] board)
+		{
+			// here we perform horizontal and vertical checks
+			for (int i = 0; i < 3; i++)
+			{
+				if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+					return true;
+				if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+					return true;
+			}
+			// here diagonal checks 
+			if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+				return true;
+			if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+				return true;
+			return false;
+		}*/
+	}
+}
